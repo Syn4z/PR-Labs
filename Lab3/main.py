7a094883445af9a1193b46f323ad09f86e6c717a
syn4z <iatcosorin797@gmail.com>
2023-09-24 11:36:23 +0300

    rafactor: change link var type; change maxPages type

--- Lab3/main.py
+++ Lab3/main.py
@@ -2,24 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def parseLinks(url, maxPages=0):
+def parseLinks(url, maxPages=None):
     linkList = []
     baseUrl = "https://999.md"
     page = 1
     try:
         while True:
-            if maxPages != 0 and page > maxPages:
+            if maxPages is not None and page > maxPages:
                 break
             response = requests.get(url + f"&page={page}")
             page += 1
             if response.status_code == 200:
                 soup = BeautifulSoup(response.text, 'html.parser')
-                links = soup.find_all("a", class_="js-item-ad")
+                pageLinks = soup.find_all("a", class_="js-item-ad")
                 maxPageLink = soup.find("a", class_="is-last-page")
-                for link in links:
-                    if link == maxPageLink:
+                for pageLink in pageLinks:
+                    if pageLink == maxPageLink:
                         break
-                    linkList.append(baseUrl + link.get('href'))
+                    linkList.append(baseUrl + pageLink.get('href'))
             else:
                 print(f"Failed to retrieve the web page for page {page}. Status code: {response.status_code}")
     except requests.exceptions.RequestException as e:
@@ -29,7 +29,7 @@ def parseLinks(url, maxPages=0):
 
 if __name__ == "__main__":
     url = "https://999.md/ro/list/real-estate/apartments-and-rooms?applied=1&eo=12900&eo=12912&eo=12885&eo=13859&ef=32&ef=33&o_33_1=776"
-    maxPages = 2
+    maxPages = 4
 
     links = parseLinks(url, maxPages)
     for link in links:
