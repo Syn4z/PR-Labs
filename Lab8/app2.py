0674b1addba34d8da52dc2ec4b3b52a1e2feaf94
syn4z <iatcosorin797@gmail.com>
2023-11-20 10:48:41 +0200

    feat: add RAFT implementation for CRUD

---
+++ Lab8/app2.py
@@ -0,0 +1,68 @@
+from flask import Flask, request
+from crud import CRUDElectroScooter
+
+service_info = {
+    "host": "127.0.0.1",
+    "port": 5001,
+    "leader": False
+}
+
+crud_electro_scooter = CRUDElectroScooter(service_info["leader"], db_url="postgresql://postgres:password1234@localhost:5432/postgres")
+
+app = Flask(__name__)
+
+
+@app.route("/api/electro-scooters", methods=["POST"])
+def create_electro_scooter():
+    headers = dict(request.headers)
+    if not crud_electro_scooter.leader and ("Token" not in headers or headers["Token"] != "Leader"):
+        return {
+            "message": "Access denied!"
+        }, 403
+    else:
+        return_dict, status_code = crud_electro_scooter.create_electro_scooter(request.json)
+        return return_dict, status_code
+
+
+@app.route("/api/electro-scooters/<string:scooter_id>", methods=["GET"])
+def get_electro_scooter_by_id(scooter_id):
+    return_dict, status_code = crud_electro_scooter.get_electro_scooter(scooter_id)
+    return return_dict, status_code
+
+
+@app.route("/api/electro-scooters", methods=["GET"])
+def get_all_electro_scooters():
+    return_dict, status_code = crud_electro_scooter.get_all_electro_scooters()
+    return return_dict, status_code
+
+
+@app.route("/api/electro-scooters/<string:scooter_id>", methods=["PUT"])
+def update_electro_scooter(scooter_id):
+    headers = dict(request.headers)
+    if not crud_electro_scooter.leader and ("Token" not in headers or headers["Token"] != "Leader"):
+        return {
+            "message": "Access denied!"
+        }, 403
+    else:
+        new_data = request.json
+        return_dict, status_code = crud_electro_scooter.update_electro_scooter(scooter_id, new_data)
+        return return_dict, status_code
+
+
+@app.route("/api/electro-scooters/<string:scooter_id>", methods=["DELETE"])
+def delete_electro_scooter(scooter_id):
+    headers = dict(request.headers)
+    if not crud_electro_scooter.leader and ("Token" not in headers or headers["Token"] != "Leader"):
+        return {
+            "message": "Access denied!"
+        }, 403
+    else:
+        return_dict, status_code = crud_electro_scooter.delete_electro_scooter(scooter_id)
+        return return_dict, status_code
+
+
+if __name__ == '__main__':
+    app.run(
+        host=service_info["host"],
+        port=service_info["port"]
+    )
